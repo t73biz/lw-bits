b9b2a58956980d73e09616f51312fad9c7fd4709
t73biz <t73biz@users.noreply.github.com>
2024-03-24 19:53:06 +0000

    Fix styling

--- src/LwBitsServiceProvider.php
+++ src/LwBitsServiceProvider.php
@@ -4,7 +4,6 @@ namespace T73biz\LwBits;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use T73biz\LwBits\Commands\LwBitsCommand;
 
 class LwBitsServiceProvider extends PackageServiceProvider
 {
--- tests/TestSupport/Components/Containers/MyDiv.php
+++ tests/TestSupport/Components/Containers/MyDiv.php
@@ -4,6 +4,6 @@ namespace T73biz\LwBits\Tests\TestSupport\Components\Containers;
 
 use T73biz\LwBits\Components\Containers\Div;
 
-class MyDiv extends Div {
-
+class MyDiv extends Div
+{
 }
