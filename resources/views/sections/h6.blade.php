f5d72f6c92fb77888ab37d9a5b6a30c3fb89534f
Ronald Chaplin <169922+t73biz@users.noreply.github.com>
2024-04-05 12:24:38 -0500

    lwb-19 Implement h6 sections component (#35)
    * lwb-10 Implement address sections component
    
    * lwb-12 Implement footer sections component
    
    * lwb-13 Implement header sections component
    
    * lwb-14 Implement h1 sections component
    
    * lwb-15 Implement h2 sections component
    
    * lwb-16 Implement h3 sections component
    
    * lwb-17 Implement h4 sections component
    
    * lwb-18 Implement h5 sections component
    
    * lwb-19 Implement h6 sections component

---
+++ resources/views/sections/h6.blade.php
@@ -0,0 +1,3 @@
+<h6 {!! $globalAttributes !!}>
+    {{ $slot }}
+</h6>
---
+++ tests/TestSupport/Components/Sections/MyH6.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace T73biz\LwBits\Tests\TestSupport\Components\Sections;
+
+use T73biz\LwBits\Components\Sections\H6;
+
+class MyH6 extends H6
+{
+}
