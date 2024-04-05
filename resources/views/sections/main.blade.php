67ffa90b08a2e6ae7a4368ef7caae117d36a574c
Ron Chaplin <ron.g.chaplin@gmail.com>
2024-04-05 12:20:33 -0500

    lwb-21 Implement main sections component

---
+++ resources/views/sections/main.blade.php
@@ -0,0 +1,3 @@
+<main {!! $globalAttributes !!}>
+    {{ $slot }}
+</main>
---
+++ tests/TestSupport/Components/Sections/MyMain.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace T73biz\LwBits\Tests\TestSupport\Components\Sections;
+
+use T73biz\LwBits\Components\Sections\Main;
+
+class MyMain extends Main
+{
+}
