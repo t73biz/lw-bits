558af69142556be88e391ee1109c8e14884e8d55
Ron Chaplin <ron.g.chaplin@gmail.com>
2024-04-05 12:20:02 -0500

    lwb-16 Implement h3 sections component

---
+++ resources/views/sections/h3.blade.php
@@ -0,0 +1,3 @@
+<h3 {!! $globalAttributes !!}>
+    {{ $slot }}
+</h3>
---
+++ tests/TestSupport/Components/Sections/MyH3.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace T73biz\LwBits\Tests\TestSupport\Components\Sections;
+
+use T73biz\LwBits\Components\Sections\H3;
+
+class MyH3 extends H3
+{
+}
