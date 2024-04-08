aa0beeb023d7c5c2e5d389355fa7cf3613f2974c
Ronald Chaplin <169922+t73biz@users.noreply.github.com>
2024-04-07 22:04:42 -0500

    lwb-126 Implement inline_text_semantics  samp component (#127)

---
+++ tests/Feature/InlineTextSemantics/SampTest.php
@@ -0,0 +1,119 @@
+<?php
+
+use Livewire\Livewire;
+use T73biz\LwBits\Tests\TestSupport\Components\InlineTextSemantics\MySamp;
+
+it('can render', function () {
+    Livewire::test(MySamp::class)
+        ->assertStatus(200);
+});
+
+it('can render with accesskey', function () {
+    Livewire::test(MySamp::class, ['accessKey' => true])
+        ->assertSee('accesskey');
+});
+
+it('can render with autocapitalize', function () {
+    Livewire::test(MySamp::class, ['autoCapitalize' => true])
+        ->assertSee('autocapitalize');
+});
+
+it('can render with autofocus', function () {
+    Livewire::test(MySamp::class, ['autoFocus' => true])
+        ->assertSee('autofocus');
+});
+
+it('can render with contenteditable', function () {
+    Livewire::test(MySamp::class, ['contentEditable' => true])
+        ->assertSee('contenteditable');
+});
+
+it('can render with css classes', function () {
+    Livewire::test(MySamp::class, ['cssClasses' => ['class1', 'class2']])
+        ->assertSee('class="class1 class2"', false);
+});
+
+it('can render with data-*', function () {
+    Livewire::test(MySamp::class, ['data' => ['name' => 'main-samp']])
+        ->assertSee('data-name="main-samp"', false);
+});
+
+it('can render with dir', function () {
+    Livewire::test(MySamp::class, ['direction' => 'rtl'])
+        ->assertSee('dir="rtl"', false);
+});
+
+it('can render with draggable', function () {
+    Livewire::test(MySamp::class, ['draggable' => true])
+        ->assertSee('draggable');
+});
+
+it('can render with enterkeyhint', function () {
+    Livewire::test(MySamp::class, ['enterKeyHint' => 'enter'])
+        ->assertSee('enterkeyhint="enter"', false);
+});
+
+it('can render with hidden', function () {
+    Livewire::test(MySamp::class, ['hidden' => true])
+        ->assertSee('hidden');
+});
+
+it('can render with id', function () {
+    Livewire::test(MySamp::class, ['id' => 'main-samp'])
+        ->assertSee('id="main-samp"', false);
+});
+
+it('can render with language', function () {
+    Livewire::test(MySamp::class, ['language' => 'en'])
+        ->assertSee('lang="en"', false);
+});
+
+it('can render with nonce', function () {
+    Livewire::test(MySamp::class, ['nonce' => 'abc123'])
+        ->assertSee('nonce="abc123"', false);
+});
+
+it('can render with parts', function () {
+    Livewire::test(MySamp::class, ['parts' => ['part1', 'part2']])
+        ->assertSee('part="part1 part2"', false);
+});
+
+it('can render with popOver', function () {
+    Livewire::test(MySamp::class, ['popOver' => true])
+        ->assertSee('popover');
+});
+
+it('can render with role', function () {
+    Livewire::test(MySamp::class, ['role' => 'main'])
+        ->assertSee('role="main"', false);
+});
+
+it('can render with slot', function () {
+    Livewire::test(MySamp::class, ['slotAttribute' => 'main-samp'])
+        ->assertSee('slot="main-samp"', false);
+});
+
+it('can render with spellcheck', function () {
+    Livewire::test(MySamp::class, ['spellCheck' => true])
+        ->assertSee('spellcheck');
+});
+
+it('can render with styles', function () {
+    Livewire::test(MySamp::class, ['styles' => ['color' => 'red', 'font-size' => '16px']])
+        ->assertSee('style="color:red;font-size:16px"', false);
+});
+
+it('can render with tabindex', function () {
+    Livewire::test(MySamp::class, ['tabIndex' => 1])
+        ->assertSee('tabindex="1"', false);
+});
+
+it('can render with title', function () {
+    Livewire::test(MySamp::class, ['title' => 'main-samp'])
+        ->assertSee('title="main-samp"', false);
+});
+
+it('can render with translate', function () {
+    Livewire::test(MySamp::class, ['translate' => true])
+        ->assertSee('translate');
+});
