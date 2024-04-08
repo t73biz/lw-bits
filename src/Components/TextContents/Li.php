a5f9dbade52d26449e976018c4649c1b7a5eaac1
Ronald Chaplin <169922+t73biz@users.noreply.github.com>
2024-04-07 20:05:45 -0500

    lwb-73 Implement li sections component (#74)
    * lwb-73 Implement li sections component
    
    * lwb-73
    Adding value attribute to Li component

---
+++ src/Components/TextContents/Li.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace T73biz\LwBits\Components\TextContents;
+
+use Illuminate\Contracts\Foundation\Application as ContractedApplication;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View as ContractedView;
+use Illuminate\Foundation\Application;
+use Illuminate\View\View;
+use Livewire\Component;
+use Livewire\Features\SupportAttributes\AttributeCollection;
+use T73biz\LwBits\Components\GlobalAttributesTrait;
+
+/**
+ * Class Li
+ */
+class Li extends Component
+{
+    use GlobalAttributesTrait;
+
+    /**
+     * This integer attribute indicates the current ordinal value of the list item as defined by the <ol> element.
+     * The only allowed value for this attribute is a number, even if the list is displayed with Roman numerals or
+     * letters. List items that follow this one continue numbering from the value set. The value attribute has no
+     * meaning for unordered lists (<ul>) or for menus (<menu>).
+     */
+    public int $value = 0;
+
+    /**
+     * Array of specific attributes for the body element.
+     */
+    private AttributeCollection $specificAttributes;
+
+    /**
+     * Standard mount function
+     */
+    public function mount(): void
+    {
+        $this->setGlobalAttributes();
+        $this->specificAttributes = new AttributeCollection();
+        if ($this->value !== 0) {
+            $this->specificAttributes->add(['value' => $this->value]);
+        }
+    }
+
+    /**
+     * Standard render function
+     */
+    public function render(): Application|ContractedApplication|ContractedView|Factory|View
+    {
+        return view(
+            'lw-bits::text_contents.li',
+            [
+                'globalAttributes' => $this->getGlobalAttributes(),
+                'specificAttributes' => $this->parseAttributes($this->specificAttributes),
+                'slot' => '',
+            ]
+        );
+    }
+}
