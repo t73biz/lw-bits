f8d28edc8599db0e0e9387b26d039cc7963f31fb
Ron Chaplin <ron.g.chaplin@gmail.com>
2024-04-07 19:37:24 -0500

    lwb-83 Implement ul sections component

---
+++ resources/views/text_contents/ul.blade.php
@@ -0,0 +1,3 @@
+<ul {!! $globalAttributes !!}>
+    {{ $slot }}
+</ul>
---
+++ tests/TestSupport/Components/TextContents/MyUl.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace T73biz\LwBits\Tests\TestSupport\Components\TextContents;
+
+use T73biz\LwBits\Components\TextContents\Ul;
+
+class MyUl extends Ul
+{
+}
