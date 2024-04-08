12e8534a00fa1e4ba7164d767577c25bce3b832b
Ronald Chaplin <169922+t73biz@users.noreply.github.com>
2024-04-07 21:49:32 -0500

    lwb-106 Implement inline_text_semantics  dfn component (#107)

---
+++ src/Components/InlineTextSemantics/Dfn.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace T73biz\LwBits\Components\InlineTextSemantics;
+
+use Illuminate\Contracts\Foundation\Application as ContractedApplication;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View as ContractedView;
+use Illuminate\Foundation\Application;
+use Illuminate\View\View;
+use Livewire\Component;
+use T73biz\LwBits\Components\GlobalAttributesTrait;
+
+/**
+ * Class Dfn
+ */
+class Dfn extends Component
+{
+    use GlobalAttributesTrait;
+
+    /**
+     * Standard mount function
+     */
+    public function mount(): void
+    {
+        $this->setGlobalAttributes();
+    }
+
+    /**
+     * Standard render function
+     */
+    public function render(): Application|ContractedApplication|ContractedView|Factory|View
+    {
+        return view(
+            'lw-bits::inline_text_semantics.dfn',
+            [
+                'globalAttributes' => $this->getGlobalAttributes(),
+                'slot' => '',
+            ]
+        );
+    }
+}
---
+++ tests/TestSupport/Components/InlineTextSemantics/MyDfn.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace T73biz\LwBits\Tests\TestSupport\Components\InlineTextSemantics;
+
+use T73biz\LwBits\Components\InlineTextSemantics\Dfn;
+
+class MyDfn extends Dfn
+{
+}
