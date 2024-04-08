b5b43ce3794af11a31befdadd7fb7059fbad9591
Ronald Chaplin <169922+t73biz@users.noreply.github.com>
2024-04-07 21:53:42 -0500

    lwb-110 Implement inline_text_semantics  italic component (#111)
    * lwb-110 Implement inline_text_semantics  italic component
    
    * lwb-110 Converting italic element to i in the blade file

---
+++ resources/views/inline_text_semantics/italic.blade.php
@@ -0,0 +1,3 @@
+<i {!! $globalAttributes !!}>
+    {{ $slot }}
+</i>
---
+++ tests/Feature/InlineTextSemantics/ItalicTest.php
@@ -0,0 +1,119 @@
+<?php
+
+use Livewire\Livewire;
+use T73biz\LwBits\Tests\TestSupport\Components\InlineTextSemantics\MyItalic;
+
+it('can render', function () {
+    Livewire::test(MyItalic::class)
+        ->assertStatus(200);
+});
+
+it('can render with accesskey', function () {
+    Livewire::test(MyItalic::class, ['accessKey' => true])
+        ->assertSee('accesskey');
+});
+
+it('can render with autocapitalize', function () {
+    Livewire::test(MyItalic::class, ['autoCapitalize' => true])
+        ->assertSee('autocapitalize');
+});
+
+it('can render with autofocus', function () {
+    Livewire::test(MyItalic::class, ['autoFocus' => true])
+        ->assertSee('autofocus');
+});
+
+it('can render with contenteditable', function () {
+    Livewire::test(MyItalic::class, ['contentEditable' => true])
+        ->assertSee('contenteditable');
+});
+
+it('can render with css classes', function () {
+    Livewire::test(MyItalic::class, ['cssClasses' => ['class1', 'class2']])
+        ->assertSee('class="class1 class2"', false);
+});
+
+it('can render with data-*', function () {
+    Livewire::test(MyItalic::class, ['data' => ['name' => 'main-italic']])
+        ->assertSee('data-name="main-italic"', false);
+});
+
+it('can render with dir', function () {
+    Livewire::test(MyItalic::class, ['direction' => 'rtl'])
+        ->assertSee('dir="rtl"', false);
+});
+
+it('can render with draggable', function () {
+    Livewire::test(MyItalic::class, ['draggable' => true])
+        ->assertSee('draggable');
+});
+
+it('can render with enterkeyhint', function () {
+    Livewire::test(MyItalic::class, ['enterKeyHint' => 'enter'])
+        ->assertSee('enterkeyhint="enter"', false);
+});
+
+it('can render with hidden', function () {
+    Livewire::test(MyItalic::class, ['hidden' => true])
+        ->assertSee('hidden');
+});
+
+it('can render with id', function () {
+    Livewire::test(MyItalic::class, ['id' => 'main-italic'])
+        ->assertSee('id="main-italic"', false);
+});
+
+it('can render with language', function () {
+    Livewire::test(MyItalic::class, ['language' => 'en'])
+        ->assertSee('lang="en"', false);
+});
+
+it('can render with nonce', function () {
+    Livewire::test(MyItalic::class, ['nonce' => 'abc123'])
+        ->assertSee('nonce="abc123"', false);
+});
+
+it('can render with parts', function () {
+    Livewire::test(MyItalic::class, ['parts' => ['part1', 'part2']])
+        ->assertSee('part="part1 part2"', false);
+});
+
+it('can render with popOver', function () {
+    Livewire::test(MyItalic::class, ['popOver' => true])
+        ->assertSee('popover');
+});
+
+it('can render with role', function () {
+    Livewire::test(MyItalic::class, ['role' => 'main'])
+        ->assertSee('role="main"', false);
+});
+
+it('can render with slot', function () {
+    Livewire::test(MyItalic::class, ['slotAttribute' => 'main-italic'])
+        ->assertSee('slot="main-italic"', false);
+});
+
+it('can render with spellcheck', function () {
+    Livewire::test(MyItalic::class, ['spellCheck' => true])
+        ->assertSee('spellcheck');
+});
+
+it('can render with styles', function () {
+    Livewire::test(MyItalic::class, ['styles' => ['color' => 'red', 'font-size' => '16px']])
+        ->assertSee('style="color:red;font-size:16px"', false);
+});
+
+it('can render with tabindex', function () {
+    Livewire::test(MyItalic::class, ['tabIndex' => 1])
+        ->assertSee('tabindex="1"', false);
+});
+
+it('can render with title', function () {
+    Livewire::test(MyItalic::class, ['title' => 'main-italic'])
+        ->assertSee('title="main-italic"', false);
+});
+
+it('can render with translate', function () {
+    Livewire::test(MyItalic::class, ['translate' => true])
+        ->assertSee('translate');
+});
