4666f14d841898b6a0befd0637b4217820e77ef0
t73biz <t73biz@users.noreply.github.com>
2024-07-09 20:35:42 +0000

    Fix styling

--- tests/TestSupport/Components/DocumentMetadata/MyTitle.php
+++ tests/TestSupport/Components/DocumentMetadata/MyTitle.php
@@ -4,6 +4,4 @@ namespace T73biz\LwBits\Tests\TestSupport\Components\DocumentMetadata;
 
 use T73biz\LwBits\Components\DocumentMetadata\Title;
 
-class MyTitle extends Title
-{
-}
+class MyTitle extends Title {}
