1dbcb31ecfba3e29a7452e398efa1a117b378b7e
t73biz <t73biz@users.noreply.github.com>
2024-07-12 17:19:43 +0000

    Fix styling

--- tests/TestSupport/Components/ImageMultimedia/MyArea.php
+++ tests/TestSupport/Components/ImageMultimedia/MyArea.php
@@ -4,6 +4,4 @@ namespace T73biz\LwBits\Tests\TestSupport\Components\ImageMultimedia;
 
 use T73biz\LwBits\Components\ImageMultimedia\Area;
 
-class MyArea extends Area
-{
-}
+class MyArea extends Area {}
