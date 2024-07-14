5bc6ac371b963b58beb1262c4552f14449979f55
Ron Chaplin <ron.g.chaplin@gmail.com>
2024-07-13 20:43:34 -0500

    lwb-245 Implement forms  option component

---
+++ resources/views/forms/option.blade.php
@@ -0,0 +1,3 @@
+<option {!! $globalAttributes !!}>
+    {{ $slot }}
+</option>
---
+++ tests/TestSupport/Components/Forms/MyOption.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace T73biz\LwBits\Tests\TestSupport\Components\Forms;
+
+use T73biz\LwBits\Components\Forms\Option;
+
+class MyOption extends Option {}
