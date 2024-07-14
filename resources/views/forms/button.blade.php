3fb2de5bd8ea2be36c1c51cf5530b141a6d62da4
Ron Chaplin <ron.g.chaplin@gmail.com>
2024-07-13 20:40:54 -0500

    lwb-227 Implement forms  button component

---
+++ resources/views/forms/button.blade.php
@@ -0,0 +1,3 @@
+<button {!! $globalAttributes !!}>
+    {{ $slot }}
+</button>
---
+++ src/Components/Forms/Button.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace T73biz\LwBits\Components\Forms;
+
+use Illuminate\Contracts\Foundation\Application as ContractedApplication;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View as ContractedView;
+use Illuminate\Foundation\Application;
+use Illuminate\View\View;
+use Livewire\Component;
+use T73biz\LwBits\Components\GlobalAttributesTrait;
+
+/**
+ * Class Button
+ */
+class Button extends Component
+{
+    use GlobalAttributesTrait;
+
+    /**
+     * Standard mount function
+     */
+    public function mount(): void
+    {
+        $this->setGlobalAttributes();
+    }
+
+    /**
+     * Standard render function
+     */
+    public function render(): Application|ContractedApplication|ContractedView|Factory|View
+    {
+        return view(
+            'lw-bits::forms.button',
+            [
+                'globalAttributes' => $this->getGlobalAttributes(),
+                'slot' => '',
+            ]
+        );
+    }
+}
