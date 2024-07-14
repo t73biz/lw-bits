7231612ddb7a827086a9d555475b84141c0e3ba1
Ronald Chaplin <169922+t73biz@users.noreply.github.com>
2024-07-14 02:19:20 -0500

    lwb-255 Implement interactive  details component (#256)
    * lwb-255 Implement interactive  details component
    
    * LWB-255 Adding specific attributes to the details component

---
+++ tests/Feature/Interactive/DetailsTest.php
@@ -0,0 +1,129 @@
+<?php
+
+use Livewire\Livewire;
+use T73biz\LwBits\Tests\TestSupport\Components\Interactive\MyDetails;
+
+it('can render', function () {
+    Livewire::test(MyDetails::class)
+        ->assertStatus(200);
+});
+
+it('can render with accesskey', function () {
+    Livewire::test(MyDetails::class, ['accessKey' => true])
+        ->assertSee('accesskey');
+});
+
+it('can render with autocapitalize', function () {
+    Livewire::test(MyDetails::class, ['autoCapitalize' => true])
+        ->assertSee('autocapitalize');
+});
+
+it('can render with autofocus', function () {
+    Livewire::test(MyDetails::class, ['autoFocus' => true])
+        ->assertSee('autofocus');
+});
+
+it('can render with contenteditable', function () {
+    Livewire::test(MyDetails::class, ['contentEditable' => true])
+        ->assertSee('contenteditable');
+});
+
+it('can render with css classes', function () {
+    Livewire::test(MyDetails::class, ['cssClasses' => ['class1', 'class2']])
+        ->assertSee('class="class1 class2"', false);
+});
+
+it('can render with data-*', function () {
+    Livewire::test(MyDetails::class, ['data' => ['name' => 'main-details']])
+        ->assertSee('data-name="main-details"', false);
+});
+
+it('can render with dir', function () {
+    Livewire::test(MyDetails::class, ['direction' => 'rtl'])
+        ->assertSee('dir="rtl"', false);
+});
+
+it('can render with draggable', function () {
+    Livewire::test(MyDetails::class, ['draggable' => true])
+        ->assertSee('draggable');
+});
+
+it('can render with enterkeyhint', function () {
+    Livewire::test(MyDetails::class, ['enterKeyHint' => 'enter'])
+        ->assertSee('enterkeyhint="enter"', false);
+});
+
+it('can render with hidden', function () {
+    Livewire::test(MyDetails::class, ['hidden' => true])
+        ->assertSee('hidden');
+});
+
+it('can render with id', function () {
+    Livewire::test(MyDetails::class, ['id' => 'main-details'])
+        ->assertSee('id="main-details"', false);
+});
+
+it('can render with language', function () {
+    Livewire::test(MyDetails::class, ['language' => 'en'])
+        ->assertSee('lang="en"', false);
+});
+
+it('can render with nonce', function () {
+    Livewire::test(MyDetails::class, ['nonce' => 'abc123'])
+        ->assertSee('nonce="abc123"', false);
+});
+
+it('can render with parts', function () {
+    Livewire::test(MyDetails::class, ['parts' => ['part1', 'part2']])
+        ->assertSee('part="part1 part2"', false);
+});
+
+it('can render with popOver', function () {
+    Livewire::test(MyDetails::class, ['popOver' => true])
+        ->assertSee('popover');
+});
+
+it('can render with role', function () {
+    Livewire::test(MyDetails::class, ['role' => 'main'])
+        ->assertSee('role="main"', false);
+});
+
+it('can render with slot', function () {
+    Livewire::test(MyDetails::class, ['slotAttribute' => 'main-details'])
+        ->assertSee('slot="main-details"', false);
+});
+
+it('can render with spellcheck', function () {
+    Livewire::test(MyDetails::class, ['spellCheck' => true])
+        ->assertSee('spellcheck');
+});
+
+it('can render with styles', function () {
+    Livewire::test(MyDetails::class, ['styles' => ['color' => 'red', 'font-size' => '16px']])
+        ->assertSee('style="color:red;font-size:16px"', false);
+});
+
+it('can render with tabindex', function () {
+    Livewire::test(MyDetails::class, ['tabIndex' => 1])
+        ->assertSee('tabindex="1"', false);
+});
+
+it('can render with title', function () {
+    Livewire::test(MyDetails::class, ['title' => 'main-details'])
+        ->assertSee('title="main-details"', false);
+});
+
+it('can render with translate', function () {
+    Livewire::test(MyDetails::class, ['translate' => true])
+        ->assertSee('translate');
+});
+
+it('can render with open', function () {
+    Livewire::test(MyDetails::class, ['open' => true])
+        ->assertSee('open');
+});
+
+it('can render with name', function () {
+    Livewire::test(MyDetails::class, ['name' => 'main-details'])
+        ->assertSee('name="main-details"', false);
+});
