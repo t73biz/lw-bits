6cc47f1060b9d9fac8b8acc8a7702757adab6915
Ronald Chaplin <169922+t73biz@users.noreply.github.com>
2024-07-14 03:13:40 -0500

    lwb-245 Implement forms  option component (#246)

---
+++ src/Components/Forms/Option.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace T73biz\LwBits\Components\Forms;
+
+use Illuminate\Contracts\Foundation\Application as ContractedApplication;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View as ContractedView;
+use Illuminate\Foundation\Application;
+use Illuminate\View\View;
+use Livewire\Component;
+use T73biz\LwBits\Components\GlobalAttributesTrait;
+
+/**
+ * Class Option
+ */
+class Option extends Component
+{
+    use GlobalAttributesTrait;
+
+    /**
+     * Standard mount function
+     */
+    public function mount(): void
+    {
+        $this->setGlobalAttributes();
+    }
+
+    /**
+     * Standard render function
+     */
+    public function render(): Application|ContractedApplication|ContractedView|Factory|View
+    {
+        return view(
+            'lw-bits::forms.option',
+            [
+                'globalAttributes' => $this->getGlobalAttributes(),
+                'slot' => '',
+            ]
+        );
+    }
+}
